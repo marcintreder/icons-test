5859018f667ae812394ccac8c6bd2edf3496decf
Marcin Treder <marcin@uxpin.com>
2017-05-08 16:54:05 -0700

    SVG src full test

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 //import PngBase from './IconsComponents/png-base.js' // png loaded through base64 links
 //import Png from './IconsComponents/png.js' // png loaded as individual files
 //import SvgBase from './IconsComponents/svg-base.js' // svg loaded through base64 links
-//import Svg from './IconsComponents/svg.js' // svg loaded as individual files
-import SvgInline from './IconsComponents/svg-inline.js' // svg loaded as individual files
+import Svg from './IconsComponents/svg.js' // svg loaded as individual files
+//import SvgInline from './IconsComponents/svg-inline.js' // svg loaded as individual files
 //import SvgIconsComponent from './IconsComponents/svg-icon-component.js'
 //import PngSprite from './IconsComponents/png-sprite.js'
 //import SvgSprite from './IconsComponents/svg-sprite.js'
@@ -16,7 +16,7 @@ export default class App extends Component {
 
     return(
       <div className='b-wrapper'>
-        <SvgInline />
+        <Svg />
       </div>
     )
   }
