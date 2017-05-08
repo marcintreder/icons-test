4dd782db7b4d6ee14276b51452cdfb2ff8098e7f
Marcin Treder <marcin@uxpin.com>
2017-05-08 14:33:19 -0700

    changing server config

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8080;
+
 const app = express();
 
-app.use(express.static(__dirname));
+// Server routes...
+app.get('/hello', (req, res) => res.send({ hi: 'there' }));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './build/index.html'))
-});
+if (process.env.NODE_ENV !== 'production') {
+  const webpack = require('webpack');
+  const webpackConfig = require('./webpack.config.js');
+  app.use(webpackMiddleware(webpack(webpackConfig)));
+} else {
+  app.use(express.static('dist'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+}
 
-app.listen(port, function() {
-    console.log("App is running on port " + port);
-});
+app.listen(process.env.PORT || 3050, () => console.log('Listening'));
