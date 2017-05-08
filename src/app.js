98ec9aa2726a77e302c5c506028344c685f11698
Marcin Treder <marcin@uxpin.com>
2017-05-08 16:07:44 -0700

    SVG Sprite test

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 //import Png from './IconsComponents/png.js' // png loaded as individual files
 //import SvgBase from './IconsComponents/svg-base.js' // svg loaded through base64 links
 //import Svg from './IconsComponents/svg.js' // svg loaded as individual files
-// import SvgInline from './IconsComponents/svg-inline.js' // svg loaded as individual files
-// import SvgIconsComponent from './IconsComponents/svg-icon-component.js'
+//import SvgInline from './IconsComponents/svg-inline.js' // svg loaded as individual files
+//import SvgIconsComponent from './IconsComponents/svg-icon-component.js'
 //import PngSprite from './IconsComponents/png-sprite.js'
-//import SvgSprite from './IconsComponents/svg-sprite.js'
-import IconFont from './IconsComponents/icon-font.js'
+import SvgSprite from './IconsComponents/svg-sprite.js'
+//import IconFont from './IconsComponents/icon-font.js'
 
 
 export default class App extends Component {
